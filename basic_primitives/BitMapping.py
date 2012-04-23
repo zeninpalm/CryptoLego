311586393996c619953b58157160c86d2c1cacec
Wei Yi <yiwei.in.cyber@gmail.com>
2012-04-23 18:05:14 +0800

    Change BitMapping to be a subclass of dict class. BitMapping only allows bitstring and intVal values and keys. Currently BitMapping has the same interface as dict objects.
    BitMappingDocTest is a simple doc test

--- basic_primitives/BitMapping.py
+++ basic_primitives/BitMapping.py
@@ -4,79 +4,26 @@ Such as the S-Boxes in DES, substitution in SPN network.
 """
 from BitVector import *
 
-class BitMapping:
-    def __init__(self):
-        self._mapping_pairs = {}
+class BitMapping(dict):
 
-    #Section of Access
-    def fill_with_pairs(self, mapping_pairs):
-        """
-        Require:    mapping_pairs != None
-        Ensure:     foreach (from,to) in mapping_pair:
-                        self.map(from) == to
-        """
-        for (from_bits, to_bits) in mapping_pairs:
-            self._mapping_pairs[from_bits] = BitVector(bitstring=to_bits)
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
 
-    def extend_with_pair(self, mapping_pair):
-        """
-        Require:    mapping_pair != None
-                    len(mapping_pair[0] == len(mapping_pair[1])
-        Ensure:     len(_mapping_pair) == len(old _mapping_pair)+1
-        """
-        (from_bits, to_bits) = mapping_pair
-        self._mapping_pairs[from_bits] = BitVector(bitstring=to_bits)
+    def __setitem__(self, key, val):
+        if isinstance(val, type('')): #bitstring specified
+            dict.__setitem__(self, key, BitVector(bitstring=val))
 
+        elif isinstance(val, type(0)): #intVal specified
+            dict.__setitem__(self, key, BitVector(intVal=val))
 
-    def set_with_table(self, mapping_table):
-        """
-        Require:    Table_not_none: mapping_table != None
-                    No_none_row: [row != None for row in table]
-        Ensure:
-            foreach row in table:
-                self.map(row.index) == row #The index of row is the source bit pattern
-        """
-        pass
+        else:
+            raise ValueError('''BitMapping constructor can only be
+                                called with bitstring or integer value''')
 
-    def put(self, from_bitstring, to_bitstring):
-        """
-        Require:    from_bitstring != None
-                    to_bitstring != None
-        Ensure:     self.map(from_bitstring) == to_bitstring
-        """
-        self._mapping_pairs[from_bitstring] = BitVector(to_bitstring)
+    def update(self, *args, **kwargs):
+        for k, v in dict(*args, **kwargs).iteritems():
+            self[k] = v
 
-    def remove(self, from_bitstring):
-        """
-        Require:    from_bitstring != None
-                    self.contains(from_bitstring)
-        Ensure:     !self.contains(from_bitstring)
-        """
-        del self._mapping_pairs[from_bitstring]
-
-    def map(self, from_bitstring):
-        """
-        Require:    Bit_pattern_valid: is_valid(bit_pattern)
-        Ensure:     Correctly_mapped
-        """
-        pass
-    # End of Access category
-
-    # Query
-    def is_valid(self, bitstring):
-        """
-        Require:    bit_pattern != None
-        Ensure:     (Result == True) or (Result == False)
-        """
-        pass
-
-    def dump_mapping(self):
-        """
-        Require:    True
-        Ensure:     Return the internal mapping table as a hash-table data structure
-        """
-        for (k, v) in self._mapping_pairs.iteritems():
-            print "%s --> %s" % (k, v)
 
 if __name__ == '__main__':
     import doctest
