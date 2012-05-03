d03e6d2ed02e3882e8cd582f99401c1369648ec7
Wei Yi <yiwei.in.cyber@gmail.com>
2012-05-03 12:24:27 +0800

    DataSource contains provide_data, while DataTarget contains set_input, every class intending to take input or output should inherit from these two classes

---
+++ basic_primitives/DataSource.py
@@ -0,0 +1,5 @@
+__author__ = 'ThinkPad'
+
+class DataSource:
+    def provide_data(self, output_type):
+        pass
---
+++ basic_primitives/DataTarget.py
@@ -0,0 +1,9 @@
+__author__ = 'ThinkPad'
+
+class DataTarget:
+    def set_input(self, data_source, input_type):
+        self._data_source = data_source
+
+    def apply(self):
+        pass
+
