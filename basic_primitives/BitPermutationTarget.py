c8e286f1f5ed10fed0ab5cf4af26090592da63a6
Wei Yi <yiwei.in.cyber@gmail.com>
2012-05-08 22:01:22 +0800

    Remove DataSource/Target, BitStringSource/Target, Input/OutputDataType, Work/DataFlow, since they are replaced by signal/slots now. And *DocTest.txt are removed.

--- basic_primitives/BitPermutationTarget.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'ThinkPad'
-import DataTarget
-
-class BitPermutationTarget(DataTarget.DataTarget):
-    pass
--- basic_primitives/OutputDataType.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'ThinkPad'
-
-class OutputDataType(object):
-    bit_string = 0
-    group_element = 1
-    ring_element = 2
-    field_element = 3
-    ecc_element = 4
-    integer = 5
